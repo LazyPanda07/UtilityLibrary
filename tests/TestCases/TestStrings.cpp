403023d8e31d6a4bcb30b51e240617de9a0c40ce
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-11-28 17:07:57 +0500

    Test

--- tests/TestCases/TestStrings.cpp
+++ tests/TestCases/TestStrings.cpp
@@ -8,13 +8,13 @@ TEST(TestStrings, Replace)
 {
     string data = "aaabbbcccaaabbbccc";
 
-    ASSERT_TRUE(utility::strings::replaceAll(data, "aaa", "x") == "xbbbccc");
-    ASSERT_TRUE(utility::strings::replaceAll(data, "bbb", "x") == "aaaxccc");
-    ASSERT_TRUE(utility::strings::replaceAll(data, "ccc", "x") == "aaabbbx");
+    ASSERT_TRUE(utility::strings::replaceAll(data, "aaa", "x") == "xbbbcccxbbbccc");
+    ASSERT_TRUE(utility::strings::replaceAll(data, "bbb", "x") == "aaaxcccaaaxccc");
+    ASSERT_TRUE(utility::strings::replaceAll(data, "ccc", "x") == "aaabbbxaaabbbx");
     
     utility::strings::replaceAllByRef(data, "aaa", "x");
     utility::strings::replaceAllByRef(data, "bbb", "y");
     utility::strings::replaceAllByRef(data, "ccc", "z");
 
-    ASSERT_TRUE(data == "xyz");
+    ASSERT_TRUE(data == "xyzxyz");
 }
