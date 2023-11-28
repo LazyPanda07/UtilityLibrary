ed4205c3ce5be56c784e611f04991294d6af580c
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-11-28 17:03:35 +0500

    Fix

--- tests/TestCases/TestStrings.cpp
+++ tests/TestCases/TestStrings.cpp
@@ -2,4 +2,19 @@
 
 #include "Strings.h"
 
+using namespace std;
 
+TEST(TestStrings, Replace)
+{
+    string data = "aaabbbcccaaabbbccc";
+
+    ASSERT_TRUE(utility::strings::replaceAll(data, "aaa", "x") == "xbbbccc");
+    ASSERT_TRUE(utility::strings::replaceAll(data, "bbb", "x") == "aaaxccc");
+    ASSERT_TRUE(utility::strings::replaceAll(data, "ccc", "x") == "aaabbbx");
+    
+    utility::strings::replaceAllByRef(data, "aaa", "x");
+    utility::strings::replaceAllByRef(data, "bbb", "y");
+    utility::strings::replaceAllByRef(data, "ccc", "z");
+
+    ASSERT_TRUE(data == "xyz");
+}
