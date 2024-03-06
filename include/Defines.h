64e4393e77045d440bcbeaa081424439bc0cbb45
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-03-06 23:16:57 +0500

    Add linux support

--- include/Defines.h
+++ include/Defines.h
@@ -3,10 +3,14 @@
 #include <string_view>
 
 #ifdef UTILITY_LIBRARY_DLL
+#ifdef __LINUX__
+#define UTILITY_LIBRARY_API __attribute__((visibility("default")))
+#else
 #define UTILITY_LIBRARY_API __declspec(dllexport)
 
 #pragma warning(disable: 4190)
 #pragma warning(disable: 4251)
+#endif
 #else
 #define UTILITY_LIBRARY_API
 #endif // UTILITY_LIBRARY_DLL
@@ -14,4 +18,4 @@
 /**
  * @brief Current version of UtilityLibrary
 */
-static constexpr std::string_view utilityLibraryVersion = "0.3.0";
+static constexpr std::string_view utilityLibraryVersion = "0.4.0";
