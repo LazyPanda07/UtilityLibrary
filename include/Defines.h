9bd50f1399fe2827dbad7b675b2b359c24ba1b49
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-07-31 20:19:34 +0500

    Fix

--- include/Defines.h
+++ include/Defines.h
@@ -23,4 +23,4 @@
 /**
  * @brief Current version of UtilityLibrary
 */
-static constexpr std::string_view utilityLibraryVersion = "0.5.3";
+static constexpr std::string_view utilityLibraryVersion = "0.5.4";
